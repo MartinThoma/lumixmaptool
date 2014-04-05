db151efab616078e4f790515a900a84d1cf4f7e6
Martin Thoma <info@martin-thoma.de>
2014-04-05 23:42:47 +0200

    deb packages create

---
+++ panasonic-maptool-1.0/debian/maptool/usr/share/maptool/maptool.py
@@ -0,0 +1,155 @@
+#!/usr/bin/env python
+
+"""
+Author: Martin Thoma <info@martin-thoma.de>,
+        based on https://github.com/RolandKluge/de.rolandkluge.lumix_map_tool/
+        from Roland Kluge
+        
+Manage GPS information for Panasonic Lumix cameras.
+
+Panasonic offers GPS metadata to add to a SD card. This metadata can contain
+tourist information that might be useful for sightseeing. This maptool helps
+to copy the data from Lumix DVD to the SD card that is inserted into your 
+computer (the camera has not to be connected).
+"""
+
+from argparse import ArgumentParser, RawTextHelpFormatter, Action
+import os
+import re
+from pyparsing import Word, alphas, nums, OneOrMore, alphanums
+import shutil
+import logging
+logging.basicConfig(filename='maptool.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+
+__version__ = "1.0"
+
+region_mapping = {}
+region_mapping[1] = 'Japan'
+region_mapping[2] = 'South Asia, Southeast Asia'
+region_mapping[3] = 'Oceania'
+region_mapping[4] = 'North America, Central America'
+region_mapping[5] = 'South America'
+region_mapping[6] = 'Northern Europe'
+region_mapping[7] = 'Eastern Europe'
+region_mapping[8] = 'Western Europe'
+region_mapping[9] = 'West Asia, Africa'
+region_mapping[10] = 'Russia, North Asia'
+
+def is_valid_mapdata(parser, path_to_mapdata):
+    """Check if path_to_mapdata is a valid path."""
+    if os.path.isfile(path_to_mapdata):
+        return path_to_mapdata
+    else:
+        parser.error("The file '%s' does not exist." % path_to_mapdata)
+
+def is_valid_sdcard(parser, path_to_sdcard):
+    """Check if sdcard is a valid path."""
+    if not os.path.exists(path_to_sdcard):
+        parser.error("The path '%s' does not exist." % path_to_sdcard)
+    if not os.access(path_to_sdcard, os.W_OK):
+        parser.error("The path '%s' is not writable" % path_to_sdcard)
+    else:
+        return path_to_sdcard
+
+def parse_mapdata(path_to_mapdata):
+    with open(path_to_mapdata, 'r') as f:
+        mapdata = f.read()
+    mapdata_pattern = re.compile("\s*(\d{8})\s*(\d{8})\s*(.*)\s*", re.DOTALL)
+    num1, num2, data = mapdata_pattern.findall(mapdata)[0]
+
+    parsed_map_data = {'num1': num1, 'num2': num2, 'regions': {}}
+    def regionParsing(x):
+        parsed_map_data['regions'][int(x[0])] = x[1:]
+
+    regionnum = Word(nums, exact=2).setResultsName("region-number").setName("region-number")
+    filename = Word(alphanums + "/.").setResultsName("filename").setName("filename")
+    regiondata = Word("{").suppress() + OneOrMore(filename) + Word("}").suppress()
+    regiondata = regiondata.setResultsName("region-data").setName("region-data")
+    region = (regionnum + regiondata).setResultsName("region").setName("region")
+    region.setParseAction(regionParsing)
+    map_grammar = OneOrMore(region)
+
+    data = data.strip() # a strange character at the end
+    map_grammar.parseString(data)
+
+    return parsed_map_data
+
+def copy_maps(path_to_mapdata, path_to_sdcard, regions):
+    """Copy map information of regions to sdcard."""
+    mapdata_cd_folder = '/'.join(path_to_mapdata.split("/")[:-1])
+    logging.info("mapdata_cd_folder: %s" % mapdata_cd_folder)
+    #mapdata_on_sdcard = path_to_sdcard + "/PRIVATE/MAP_DATA"
+    
+    # This works with Panasonic Lumix DMC TZ-41
+    mapdata_on_sdcard = os.path.join(path_to_sdcard, "PRIVATE/PANA_GRP/PAVC/LUMIX/MAP_DATA")
+    if not os.path.exists(mapdata_on_sdcard):
+        os.makedirs(mapdata_on_sdcard)
+    mapdata = parse_mapdata(path_to_mapdata)
+
+    # Delete previously stored cards
+    shutil.rmtree(mapdata_on_sdcard, ignore_errors=True)
+
+    # And create the directory again
+    os.makedirs(mapdata_on_sdcard)
+
+    for selected_region_id in regions:
+        print("Copying region '%s' ..." % selected_region_id)
+        for path in mapdata['regions'][selected_region_id]:
+            logging.info("Copy file %s" % path)
+            subdir, filename = path.split("/")
+            abspath_to_source_file = os.path.join(mapdata_cd_folder, path)
+            target_dir = mapdata_on_sdcard + "/" + subdir
+            target_file = target_dir + "/" + filename
+
+            logging.info("abspath_to_source_file: %s" % abspath_to_source_file)
+            logging.info("target_dir: %s" % target_dir)
+            logging.info("target_file: %s" % target_file)
+
+            if not os.path.exists(target_dir):
+                os.mkdir(target_dir)
+
+            if not os.path.exists(target_file):
+                shutil.copy(abspath_to_source_file, target_dir)
+        print("Copying region '%i' DONE" % selected_region_id)
+    print("All operations exited succesfully.")
+
+class UniqueAppendAction(Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        unique_values = set(values)
+        setattr(namespace, self.dest, unique_values)
+
+def autodetect_mapdata():
+    """Try to find the DVD with map data."""
+    path = "/media"
+    subdir = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
+    while len(subdir) == 1:
+        path = os.path.join(path, subdir[0])
+        subdir = [f for f in os.listdir(path) if os.path.isdir(os.path.join(path, f))]
+
+    if "MAP_DATA" in subdir:
+        path = path = os.path.join(path, "MAP_DATA/MapList.dat")
+        return path
+    return ""
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description=__doc__, formatter_class=RawTextHelpFormatter)
+
+    # Add more options if you like
+    parser.add_argument("-m", "--mapdata", dest="mapdata", metavar="MAPDATA",
+                    default=autodetect_mapdata(),
+                    help="path to MAPDATA folder on the Lumix DVD",
+                    type=lambda x: is_valid_mapdata(parser, x))
+    parser.add_argument("-s", "--sdcard", dest="path_to_sdcard", metavar="SDCARD",
+                    required=True,
+                    help="path to SDCARD",
+                    type=lambda x: is_valid_sdcard(parser, x))
+    parser.add_argument("-r", "--regions", dest="regions", nargs='+', 
+                    required=True, choices=list(range(1,11)), type=int,
+                    action=UniqueAppendAction,
+                    help="The space-separated indices of the regions to copy. " \
+                    + "E.g. 1 6 10. At least one region needs to be given. " \
+                    + "Regions are:\n" \
+                    + "\n".join(list(map(lambda i: "%i -\t%s" % (i, region_mapping[i]), range(1,11)))))
+    parser.add_argument('--version', action='version', version='%(prog)s ' + __version__)
+    args = parser.parse_args()
+    copy_maps(args.mapdata, args.path_to_sdcard, args.regions)
---
+++ panasonic-maptool_1.0-1.dsc
@@ -0,0 +1,20 @@
+Format: 3.0 (quilt)
+Source: panasonic-maptool
+Binary: panasonic-maptool
+Architecture: any
+Version: 1.0-1
+Maintainer: Martin Thoma <info@martin-thoma.de>
+Homepage: https://github.com/MartinThoma/lumix_map_tool
+Standards-Version: 3.9.4
+Build-Depends: debhelper (>= 8.0.0)
+Package-List: 
+ panasonic-maptool deb admin extra
+Checksums-Sha1: 
+ 34c6ab3b0e5fa61db2940979488bdbb188c96b87 14533 panasonic-maptool_1.0.orig.tar.gz
+ cbe126857c367b4efc28a3dd8b3b027b9f13ffff 2083 panasonic-maptool_1.0-1.debian.tar.gz
+Checksums-Sha256: 
+ d656265a0443f7b4695420e35464459735ab7fba93a34f7c3dda353b6bb77968 14533 panasonic-maptool_1.0.orig.tar.gz
+ 1876e6ba236c484cb0d088efc53d208064cc62c5eb1d216acd202d9871adbd1f 2083 panasonic-maptool_1.0-1.debian.tar.gz
+Files: 
+ 661b9bbc083b0d7b412fdc162399bf5f 14533 panasonic-maptool_1.0.orig.tar.gz
+ bc91a2209954be206258571467108c91 2083 panasonic-maptool_1.0-1.debian.tar.gz
