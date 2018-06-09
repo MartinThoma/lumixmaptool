9ae5e5fad3c0b86a14e347ce139b96751e113fac
Martin Thoma <info@martin-thoma.de>
2018-06-09 17:11:57 +0200

    MAINT: Update development status trove classifier to '7 - Inactive'

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
+# Core modules
+import io
+import os
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+
+def read(file_name):
+    """Read a text file and return the content as a string."""
+    with io.open(os.path.join(os.path.dirname(__file__), file_name),
+                 encoding='utf-8') as f:
+        return f.read()
+
 config = {
     'name': 'lumixmaptool',
-    'version': '1.0.15',
+    'version': '1.0.16',
     'author': 'Martin Thoma',
     'author_email': 'info@martin-thoma.de',
     'maintainer': 'Martin Thoma',
@@ -16,12 +26,8 @@ config = {
     'url': 'https://github.com/MartinThoma/lumixmaptool',
     'license': 'MIT',
     'description': 'Manage GPS information for Panasonic Lumix cameras.',
-    'long_description': ("Panasonic offers GPS metadata to add to a SD card. "
-                         "This metadata can contain tourist information that "
-                         "might be useful for sightseeing. \n"
-                         "This maptool helps to copy the data from Lumix DVD "
-                         "to the SD card that is inserted into your computer "
-                         "(the camera has not to be connected)."),
+    'long_description': read('README.md'),
+    'long_description_content_type': 'text/markdown',
     'install_requires': [
         "argparse >= 1.2.1",
         "pyparsing >= 2.0.1",
@@ -29,7 +35,7 @@ config = {
     ],
     'keywords': ['Lumix', 'Panasonic', 'TZ41', 'Camera', 'GPS'],
     'download_url': 'https://github.com/MartinThoma/lumixmaptool',
-    'classifiers': ['Development Status :: 3 - Alpha',
+    'classifiers': ['Development Status :: 7 - Inactive',
                     'Environment :: Console',
                     'Intended Audience :: End Users/Desktop',
                     'License :: OSI Approved :: MIT License',
